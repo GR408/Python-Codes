cfa562346e6b4d4ffa42d31d70057440bc2fb0e8
GR408 <124152737+GR408@users.noreply.github.com>
2023-12-24 18:40:04 +0100

    Add files via upload

---
+++ Lists.py
@@ -0,0 +1,7 @@
+myfavouritedesserts = ['cookies',  'cake', 'ice cream', 'donuts' ]
+carsseen = [3, 4, 4, 8]
+answers = [True, False, False, True, False]
+mix = ['cookies', 5, True]
+mix2 = [' four wheeles', 'differnt colors']
+moredesserts = ['donuts',  'cake', 'ice cream', 'cookies' ]
+print(moredesserts == myfavouritedesserts)
